fe1a5d1dcc4e24397ea66d9099ac74b1c6c7abb4
elYoha17 <yohananmiteo17@gmail.com>
2022-09-15 03:20:49 +0100

    Ajouter un controlleur

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1 class="mt-8 text-teal-700 text-3xl text-center font-semibold">Hello world !</h1>
+
+    @vite('resources/js/app.js')
+</body>
+</html>
